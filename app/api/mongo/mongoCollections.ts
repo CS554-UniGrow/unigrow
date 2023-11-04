0a320968f6db144a06c81c3763b06f478b578287
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-11-03 21:44:58 -0400

    v1

---
+++ app/api/mongo/mongoCollections.ts
@@ -0,0 +1,17 @@
+import { dbConnection } from "./mongoConnection";
+
+const getCollectionFn = (collection: string) => {
+  let _col: any = undefined; // You might want to specify a more specific type for _col
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+// Note: You will need to change the code below to have the collection required by the assignment!
+export const courses = getCollectionFn("courses");
---
+++ app/api/mongo/mongoConnection.ts
@@ -0,0 +1,26 @@
+import { MongoClient, Db } from "mongodb";
+import { mongoConfig } from "./settings";
+
+let _connection: MongoClient | undefined;
+let _db: Db | undefined;
+
+const dbConnection = async (): Promise<Db> => {
+  if (!_connection) {
+    _connection = await MongoClient.connect(mongoConfig.serverUrl);
+    _db = _connection.db(mongoConfig.database);
+  }
+
+  if (!_db) {
+    throw new Error("Database connection not established.");
+  }
+
+  return _db;
+};
+
+const closeConnection = async () => {
+  if (_connection) {
+    await _connection.close();
+  }
+};
+
+export { dbConnection, closeConnection };
