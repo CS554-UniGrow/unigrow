6e33032ccd7bb1de7140459588daff3c1782e8e2
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-11-04 15:30:20 -0400

    mind blown.

--- scripts/seed.js
+++ scripts/seed.js
@@ -2,40 +2,27 @@ import {
   dbConnection,
   closeConnection
 } from "../app/api/mongo/mongoConnection.js";
-import { courses as courseColleciton } from "../app/api/mongo//mongoCollections.js";
+import { courses as courseColleciton } from "../app/api/mongo/mongoCollections.js";
 
-const seedData = [
-  {
-    course_title: "AAI 551 Engineering Programming: Python",
-    course_description:
-      "This course presents tool, techniques, algorithms, and programming techniques using the Python programming language for data intensive applications and decision making. The course formally introduces techniques to: (i) gather,(ii) store, and (iii) process large volumes of data to make informed decisions. Such techniques find applicability in many engineering application areas, including communications systems, embedded systems, smart grids, robotics, Internet, and enterprise networks, or any network where information flows and alters decision making.",
-    course_credits: "3",
-    course_prereqs: [],
-    course_offered_in: [],
-    course_code: "AAI 551",
-    course_level: "500",
-    course_rating: null,
-    course_professors: [],
-    course_syllabus: "",
-    currently_enrolled: [],
-    previous_enrolled: [],
-    stevens_course_link:
-      "https://stevens.smartcatalogiq.com/en/2023-2024/Academic-Catalog/Courses/AAI-Applied-Artificial-Intelligence/500/AAI-551"
-  }
-  // Add more data as needed
-];
+// good to know !
+const rawData = fs.readFileSync("./scripts/course_data_extracted.json");
+const seedData = JSON.parse(rawData);
 
 export async function seed() {
-  const db = await dbConnection(); // Connect to the database
-
   try {
+    const db = await dbConnection();
+    console.log("Connected to the database.");
+
     await db.dropDatabase();
+    console.log("Database cleared.");
+
     const courses = await courseColleciton();
-    await courses.insertMany(seedData); // Insert data into the database using your model
-    console.log("Data seeded successfully");
+    await courses.insertMany(seedData);
+    console.log("Data seeded successfully.");
   } catch (error) {
     console.error("Error seeding data:", error);
   } finally {
     await closeConnection();
+    console.log("Database connection closed.");
   }
 }
