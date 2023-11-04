99ff804aeb1d5ea7d3b7c03177516484610e1994
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-11-04 16:54:37 -0400

    Update settings.ts

--- app/api/mongo/settings.ts
+++ app/api/mongo/settings.ts
@@ -1,5 +1,6 @@
 // You will need to change the DB name to match the required DB name in the assignment specs!
 export const mongoConfig = {
-  serverUrl: "mongodb://localhost:27017/",
-  database: "UniGrow"
+  serverUrl:
+    process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017/",
+  database: process.env.MONGO_DB_NAME || "UniGrow"
 };
