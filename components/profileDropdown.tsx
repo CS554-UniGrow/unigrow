401440e58fd80ceb7128f5618728748356a0fb9c
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-11-08 03:01:48 -0500

    commented due to bugs

--- components/profileDropdown.tsx
+++ components/profileDropdown.tsx
@@ -14,48 +14,52 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { ThemeToggle } from "./theme-toggle";
 
+// export default function ProfileDropdown() {
+//   const { currentUser } = useContext(UserContext);
+//   return (
+//     <DropdownMenu>
+//       <DropdownMenuTrigger asChild>
+//         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+//           <Avatar className="h-8 w-8">
+//             <AvatarImage src={currentUser?.user.photoURL} alt="@shadcn" />
+//             <AvatarFallback>SC</AvatarFallback>
+//           </Avatar>
+//         </Button>
+//       </DropdownMenuTrigger>
+//       <DropdownMenuContent className="w-56" align="end" forceMount>
+//         <DropdownMenuLabel className="flex justify-between font-normal">
+//           <div className="flex flex-col space-y-1">
+//             <p className="text-sm font-medium leading-none">
+//               {currentUser?.user.displayName}
+//             </p>
+//             <p className="text-xs leading-none text-muted-foreground">
+//               {currentUser?.user.email}
+//             </p>
+//           </div>
+//           <ThemeToggle />
+//         </DropdownMenuLabel>
+//         <DropdownMenuSeparator />
+//         <DropdownMenuGroup>
+//           <DropdownMenuItem>Rating</DropdownMenuItem>
+//           <DropdownMenuItem>
+//             Profile
+//             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+//           </DropdownMenuItem>
+//           <DropdownMenuItem>
+//             Settings
+//             <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+//           </DropdownMenuItem>
+//         </DropdownMenuGroup>
+//         <DropdownMenuSeparator />
+//         <DropdownMenuItem>
+//           Log out
+//           <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+//         </DropdownMenuItem>
+//       </DropdownMenuContent>
+//     </DropdownMenu>
+//   );
+// }
+
 export default function ProfileDropdown() {
-  const { currentUser } = useContext(UserContext);
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src={currentUser?.user.photoURL} alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="flex justify-between font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">
-              {currentUser?.user.displayName}
-            </p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {currentUser?.user.email}
-            </p>
-          </div>
-          <ThemeToggle />
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>Rating</DropdownMenuItem>
-          <DropdownMenuItem>
-            Profile
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Settings
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+  return <></>;
 }
