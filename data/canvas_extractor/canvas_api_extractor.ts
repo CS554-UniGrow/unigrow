772f174d544286771c59ca08da3792a6daeb54d3
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-11-08 02:22:12 -0500

    update

--- data/canvas_extractor/canvas_api_extractor.ts
+++ data/canvas_extractor/canvas_api_extractor.ts
@@ -1,10 +1,11 @@
 import { logger } from "@/lib/logger";
-
+import fs from "fs";
 import axios, { AxiosError } from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { Course, CourseApiReturn, UserProfile } from "@/lib/types";
 import { courseList, semesters } from "@/lib/constants";
 import { courses } from "@/config/mongo/mongoCollections";
+import path from "path";
 let domain = "https://sit.instructure.com/api/v1/";
 
 async function getUserProfileDetails(apiKey: string) {
@@ -78,42 +79,71 @@ async function extractSyllabusFromStudentCourseDetails(
     let moduleIds = response.data.map((module: any) => module.id);
 
     for (const moduleId of moduleIds) {
-      let url =
-        domain +
-        "courses/" +
-        course.course_id +
-        "/modules/" +
-        moduleId +
-        "/items";
-      let moduleItems = await axios.get(url, {
-        method: "get",
-        headers: {
-          Authorization: "Bearer " + apiKey
-        }
-      });
-
-      let content_ids = moduleItems.data
-        .map((item: any) => item.content_id) // Map to get content_ids
-        .filter((content_id: any) => content_id !== undefined); // Filter out undefined values
-
-      for (const content_id of content_ids) {
-        let url = domain + "files/" + content_id;
-        let response = await axios.get(url, {
+      try {
+        let url =
+          domain +
+          "courses/" +
+          course.course_id +
+          "/modules/" +
+          moduleId +
+          "/items";
+        let moduleItems = await axios.get(url, {
           method: "get",
           headers: {
             Authorization: "Bearer " + apiKey
           }
         });
 
-        if (response.data.filename.toLowerCase().includes("syllabus")) {
-          syllabusDoc = response.data.url;
-          let coursesCollection = await courses();
-          let result = await coursesCollection.updateOne(
-            { course_code: course.course_code },
-            { $set: { course_syllabus: syllabusDoc } }
-          );
-          logger.info(result);
+        let content_ids = moduleItems.data
+          .map((item: any) => item.content_id) // Map to get content_ids
+          .filter((content_id: any) => content_id !== undefined); // Filter out undefined values
+
+        for (const content_id of content_ids) {
+          try {
+            let url = domain + "files/" + content_id;
+            let response = await axios.get(url, {
+              method: "get",
+              headers: {
+                Authorization: "Bearer " + apiKey
+              }
+            });
+
+            if (response.data.filename.toLowerCase().includes("syllabus")) {
+              //download the syllabus from the url and store it in the database
+              let fileStreamResult = await axios.get(response.data.url, {
+                responseType: "stream"
+              });
+              let directoryPath = "./public/data/syllabus/";
+              let fileName = course.course_code.replace(" ", "_") + ".pdf";
+              if (!fs.existsSync(directoryPath)) {
+                fs.mkdirSync(directoryPath, { recursive: true });
+              }
+
+              // Combine the directory path and file name to create the full file path
+              const localFilePath = path.join(directoryPath, fileName);
+
+              // Create a write stream to store the syllabus on the server's file system
+              const writeStream = fs.createWriteStream(localFilePath);
+              fileStreamResult.data.pipe(writeStream);
+
+              // Wait for the file to be fully written
+              await new Promise((resolve) => {
+                writeStream.on("finish", resolve);
+              });
+
+              let coursesCollection = await courses();
+              let updateResult = await coursesCollection.updateOne(
+                { course_code: course.course_code },
+                { $set: { course_syllabus: directoryPath + fileName } }
+              );
+              logger.info(updateResult);
+            }
+          } catch (error) {
+            logger.error("Axios Error: " + error + " for the url " + url);
+          }
         }
+      } catch (error) {
+        logger.error("Axios Error: " + error + " for the url " + url);
       }
     }
   }
