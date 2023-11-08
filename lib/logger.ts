8f6d2d9d7878760ac35cd7adf467c51417b16fb8
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-11-08 02:38:14 -0500

    Update logger.ts

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,8 +1,15 @@
 import { pino } from "pino";
 import pretty from "pino-pretty";
+import fs from "fs";
+// ./logs/logfile.log check if exists else create
+if (!fs.existsSync("./logs" || process.env.LOG_DIR))
+  fs.mkdir("./logs", { recursive: true }, (err) => {});
 pino.stdTimeFunctions.isoTime = () => `,"time":"${new Date().toISOString()}"`;
 const logger = pino(
   { timestamp: pino.stdTimeFunctions.isoTime, mkdir: true },
-  pino.multistream([pretty(), pino.destination("./logs/logfile.log")])
+  pino.multistream([
+    pretty(),
+    pino.destination(("./logs/" || process.env.LOG_DIR) + "logfile.log")
+  ])
 );
 export { logger };
