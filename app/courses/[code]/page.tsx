3681ae0e2bef9019a3e05c5dac91fa95b96841f2
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-11-09 15:17:46 -0500

    added course level page

--- app/courses/[code]/page.tsx
+++ app/courses/[code]/page.tsx
@@ -9,164 +9,48 @@ import { departmentMapper } from "@/lib/constants";
 
 import { Button } from "@/components/ui/button";
 import { Course } from "@/lib/types";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter
+} from "@/components/ui/card";
+import Link from "next/link";
 
 const DepertmentCourses = async ({ params }: { params: { code: string } }) => {
   const { code } = params;
 
   const courses: Course[] = await getCourseByDepartment(code);
-
+  const course_level_unique = [
+    ...new Set(courses.map((course) => course.course_level))
+  ];
   return (
     <div className="mx-auto max-w-4xl">
       <h1 className="text-3xl">
-        All courses under Department of {departmentMapper[code.toUpperCase()]}
+        All courses Levels under Department of{" "}
+        {departmentMapper[code.toUpperCase()]}
       </h1>
-      {courses.map((course) => (
-        <Accordion type="single" key={course._id} collapsible>
-          <AccordionItem value="item-1">
-            <AccordionTrigger>{course.course_title}</AccordionTrigger>
-            <AccordionContent>
-              <div className="mt-6 rounded-lg border-2 border-gray-100 p-2 dark:border-slate-600">
-                <dl className="divide-y divide-gray-100 dark:divide-slate-700">
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm font-medium leading-6 ">
-                      Professor Name
-                    </dt>
-                    <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
-                      {course?.course_professors.map((professor) => (
-                        <span className="mr-4" key={professor.display_name}>
-                          {professor.display_name}
-                        </span>
-                      ))}{" "}
-                      {course?.course_professors.length === 0 && <span>-</span>}
-                    </dd>
-                  </div>
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm font-medium leading-6 ">
-                      Description
-                    </dt>
-                    <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
-                      {course?.course_description || <span>-</span>}
-                    </dd>
-                  </div>
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm font-medium leading-6 ">Credits</dt>
-                    <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
-                      {course?.course_credits}
-                    </dd>
-                  </div>
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm font-medium leading-6 ">
-                      Course Prerequisites
-                    </dt>
-                    <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
-                      {course?.course_prereqs?.map((prereq) => (
-                        <span className="mr-4" key={prereq}>
-                          {prereq}
-                        </span>
-                      ))}{" "}
-                      {course?.course_prereqs.length === 0 && <span>-</span>}
-                    </dd>
-                  </div>
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm font-medium leading-6 ">
-                      Semester Offered
-                    </dt>
-                    <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
-                      {course?.course_offered_in.map((semester) => (
-                        <span className="mr-4" key={semester}>
-                          {semester}
-                        </span>
-                      ))}{" "}
-                      {course?.course_offered_in.length === 0 && (
-                        <span>Fall Spring Summer </span>
-                      )}
-                    </dd>
-                  </div>
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm font-medium leading-6 ">
-                      Course Rating
-                    </dt>
-                    <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
-                      {course?.course_rating || 0} / 5
-                    </dd>
-                  </div>
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm font-medium leading-6 ">
-                      Students Enrolled
-                    </dt>
-                    <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
-                      {course?.currently_enrolled.map((enrolled) => (
-                        <span className="mr-4" key={enrolled}>
-                          {enrolled}
-                        </span>
-                      ))}{" "}
-                      {course?.currently_enrolled.length === 0 && (
-                        <span>-</span>
-                      )}
-                    </dd>
-                  </div>
 
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm font-medium leading-6 ">Syllabus</dt>
-                    <dd className="mt-2 text-sm  sm:col-span-2 sm:mt-0">
-                      <ul
-                        role="list"
-                        className="divide-y divide-gray-100 rounded-md border border-gray-200"
-                      >
-                        <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                          <div className="flex w-0 flex-1 items-center">
-                            <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                              <span className="truncate font-medium">
-                                {`${course.course_syllabus}`}
-                              </span>
-                              <span className="flex-shrink-0 text-gray-400">
-                                2.4mb
-                              </span>
-                            </div>
-                          </div>
-                          <div className="ml-4 flex-shrink-0">
-                            <a
-                              href={`${course.course_syllabus}`}
-                              className="flex font-medium hover:text-indigo-500"
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="lucide lucide-arrow-down-to-line"
-                              >
-                                <path d="M12 17V3" />
-                                <path d="m6 11 6 6 6-6" />
-                                <path d="M19 21H5" />
-                              </svg>
-                              Download
-                            </a>
-                          </div>
-                        </li>
-                      </ul>
-                    </dd>
-                  </div>
-                </dl>
-                <div className="flex justify-center">
-                  <a
-                    target="_blank"
-                    href={course.stevens_course_link}
-                    rel="noopener noreferrer"
-                  >
-                    <Button>View on Website</Button>
-                  </a>
-                </div>
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      ))}
+      <div className="grid grid-cols-1 gap-8 py-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {course_level_unique.map((course_level) => (
+          <Card key={course_level} className="flex flex-col justify-center">
+            <CardHeader>
+              <Link href={`/courses/${code}/${course_level}`}>
+                <CardTitle className="text-2xl">{course_level}</CardTitle>
+              </Link>
+            </CardHeader>
+            <CardContent></CardContent>
+            <CardFooter>
+              <Link href={`/courses/${code}/${course_level}`}>
+                <Button className="text-sm">
+                  View Courses in {course_level}
+                </Button>
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
