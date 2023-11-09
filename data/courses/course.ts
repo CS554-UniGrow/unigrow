5878cdb333a33184c0d704911fa336c761ba5aa9
sanjeet-jain <91623753+sanjeet-jain@users.noreply.github.com>
2023-11-09 15:18:31 -0500

    added course level page (#9)

--- data/courses/course.ts
+++ data/courses/course.ts
@@ -1,8 +1,16 @@
 import { courses as courseCollection } from "@/config/mongo/mongoCollections";
 
-export const getCourseByDepartment = async (department: string) => {
+export const getCourseByDepartment = async (
+  department: string,
+  level: string = ""
+) => {
   let departmentPattern = new RegExp("^" + department + "$", "i");
   const course = await courseCollection();
-  let courses = await course.find({ department_code: departmentPattern }).toArray();
+  let courses = await course
+    .find({ department_code: departmentPattern })
+    .toArray();
+  if (level !== "") {
+    courses = courses.filter((course: any) => course.course_level === level);
+  }
   return courses;
 };
