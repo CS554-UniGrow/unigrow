d798dc1ac2a9889babbb7accc4bb5a18c24c36a4
Dhruv Vaghela <dhruvvaghela17@gmail.com>
2023-11-09 12:41:13 -0500

    fix(userContext): added types for prod build

--- components/userComponent.tsx
+++ components/userComponent.tsx
@@ -1,8 +1,23 @@
 "use client";
 import React, { createContext, useState, useContext } from "react";
 
-export const UserContext = createContext(null);
-export const UserContextProvider = ({ children }) => {
+type UserContextType = {
+  currentUser: any;
+  setCurrentUser: (user: any) => void;
+};
+
+const initialState = {
+  currentUser: null,
+  setCurrentUser: () => {}
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const UserContext = createContext<UserContextType>(initialState);
+
+export const UserContextProvider = ({ children }: Props) => {
   const [currentUser, setCurrentUser] = useState();
 
   //Note: how can i implement here to provide context.provoder with users value without making the APi call from within here
