4c76f0889ecc9b0cb22ef79a117b9fb7857f358f
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-11-10 15:47:05 -0500

    fixes to api/questions and signup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Dashboard from "@/app/dashboard/page";
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex">
@@ -7,6 +8,9 @@ export default function Home() {
         This is the landing page. Will think a way to remove navbar from this
         component.
       </h1>
+      <Link href="/signup">
+        <Button>Signup</Button>
+      </Link>
     </main>
   );
 }
