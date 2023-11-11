fef6d51bad78075ba504e95dab86d9244fde0aca
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-11-10 23:06:26 -0500

    Update logger.ts

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,24 +1,28 @@
 import pino from "pino";
 import { logflarePinoVercel } from "pino-logflare";
+let logger = pino();
+if (process.env.VERCEL === "1") {
+  const { stream, send } = logflarePinoVercel({
+    apiKey: process.env.NEXT_PUBLIC_LOGFLARE_KEY!,
+    sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_TOKEN!
+  });
 
-const { stream, send } = logflarePinoVercel({
-  apiKey: process.env.NEXT_PUBLIC_LOGFLARE_KEY!,
-  sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_TOKEN!
-});
-
-const logger = pino(
-  {
-    browser: {
-      transmit: {
-        send: send
+  logger = pino(
+    {
+      browser: {
+        transmit: {
+          send: send
+        }
+      },
+      base: {
+        env: process.env.NODE_ENV,
+        revision: process.env.VERCEL_GITHUB_COMMIT_SHA
       }
     },
-    base: {
-      env: process.env.NODE_ENV,
-      revision: process.env.VERCEL_GITHUB_COMMIT_SHA
-    }
-  },
-  stream
-);
+    stream
+  );
+} else {
+  logger = pino({ timestamp: pino.stdTimeFunctions.isoTime, mkdir: true });
+}
 
 export default logger;
