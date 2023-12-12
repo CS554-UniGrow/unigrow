f9e611f4d582621bc4343cef004b3b6250a7f408
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-12-11 20:43:06 -0500

    v2

--- components/ui/loading.tsx
+++ components/ui/loading.tsx
@@ -4,14 +4,17 @@ import Image from "next/image";
 const Loading = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-50">
-      <div className="flex items-center space-x-1">
+      <div className="flex items-center space-x-1 rounded-lg bg-gray-700 p-4 shadow-lg">
         <Image
-          className=" animate-spin"
+          className="animate-spin"
           width={100}
           height={100}
           alt="Loading..."
           src={loadingLogo}
         />
+        <p className="text-white">
+          Loading... Please do not refresh or go back
+        </p>
       </div>
     </div>
   );
