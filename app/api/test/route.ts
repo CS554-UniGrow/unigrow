61cc8c416a14a8011d4ac9ddda67828462200bce
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-12-11 20:55:50 -0500

    Update route.ts

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -3,6 +3,5 @@ import { courses as courseColleciton } from "@/config/mongo/mongoCollections";
 import { getAllUsersCourseDetails } from "@/data/canvas_extractor/canvas_api_extractor";
 export async function GET(request: NextRequest, response: NextResponse) {
   await getAllUsersCourseDetails();
-  const courses = await courseColleciton();
-  return NextResponse.json({ courses: await courses.find({}).toArray() });
+  return NextResponse.json({ completed: true });
 }
