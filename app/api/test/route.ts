d2f7d2f880d496a689136492a72d2457d369ad8b
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-12-14 01:38:14 -0500

    Update route.ts

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,6 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { courses as courseColleciton } from "@/config/mongo/mongoCollections";
 import { getAllUsersCourseDetails } from "@/data/canvas_extractor/canvas_api_extractor";
 export async function GET(request: NextRequest, response: NextResponse) {
-  await getAllUsersCourseDetails();
+  //await getAllUsersCourseDetails();
   return NextResponse.json({ completed: true });
 }
