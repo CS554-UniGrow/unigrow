c63ebe05b6af0f98eac29b6c3b19c93b2c73d114
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-12-14 00:07:48 -0500

    lint fixes

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -6,123 +6,167 @@ import Link from "next/link";
 export default function About() {
   return (
     <main>
-        {/* Page Header */}
-        <section className="page-header-section container mx-auto text-center py-24 bg-[rgba(0,0,0,0.5)] my-8 p-8">
-            <h1 className="text-5xl font-bold mb-6">About UniGrow</h1>
-            <p className="text-xl mb-8">
-            Discover the vision, team, and passion behind UniGrow.
-            </p>
-        </section>
+      {/* Page Header */}
+      <section className="page-header-section container mx-auto my-8 bg-[rgba(0,0,0,0.5)] p-8 py-24 text-center">
+        <h1 className="mb-6 text-5xl font-bold">About UniGrow</h1>
+        <p className="mb-8 text-xl">
+          Discover the vision, team, and passion behind UniGrow.
+        </p>
+      </section>
 
-        {/* Our Vision */}
-        <section className="our-vision-section text-center container mx-auto bg-[rgba(0,0,0,0.5)] my-8 p-8">
+      {/* Our Vision */}
+      <section className="our-vision-section container mx-auto my-8 bg-[rgba(0,0,0,0.5)] p-8 text-center">
         <div className="container mx-auto">
-            <h2 className="text-3xl font-bold  text-white mb-4">Our Vision</h2>
-            <p className="text-xl text-white mb-6">
-            At UniGrow, we aim to revolutionize the academic experience by connecting students and educators on a dynamic platform for growth and collaboration. We believe that education should be accessible, inclusive, and empowering.
-            </p>
-            <p className="text-xl text-white mb-6">
-            Our commitment extends beyond providing educational resources. We strive to create a nurturing environment where students can share insights, educators can disseminate knowledge, and together, build a community that values learning and innovation.
-            </p>
-            <p className="text-xl text-white mb-6">
-            With a focus on intuitive technology and user-centric design, UniGrow facilitates a seamless integration of academic tools and social connectivity. We're not just building a platform; we're fostering a movement towards a brighter, more connected future in education.
-            </p>
+          <h2 className="mb-4 text-3xl  font-bold text-white">Our Vision</h2>
+          <p className="mb-6 text-xl text-white">
+            At UniGrow, we aim to revolutionize the academic experience by
+            connecting students and educators on a dynamic platform for growth
+            and collaboration. We believe that education should be accessible,
+            inclusive, and empowering.
+          </p>
+          <p className="mb-6 text-xl text-white">
+            Our commitment extends beyond providing educational resources. We
+            strive to create a nurturing environment where students can share
+            insights, educators can disseminate knowledge, and together, build a
+            community that values learning and innovation.
+          </p>
+          <p className="mb-6 text-xl text-white">
+            With a focus on intuitive technology and user-centric design,
+            UniGrow facilitates a seamless integration of academic tools and
+            social connectivity. We&apos;re not just building a platform;
+            we&apos;re fostering a movement towards a brighter, more connected
+            future in education.
+          </p>
         </div>
-        </section>
+      </section>
 
-        <section className="our-values-section container text-center mx-auto bg-[rgba(0,0,0,0.5)] my-8 p-8">
+      <section className="our-values-section container mx-auto my-8 bg-[rgba(0,0,0,0.5)] p-8 text-center">
         <div className="container mx-auto">
-            <h2 className="text-3xl font-bold text-white mb-8">Our Values</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <h2 className="mb-8 text-3xl font-bold text-white">Our Values</h2>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
             {/* Value: Innovation */}
-            <div className="value-card bg-white p-4 rounded-lg shadow-lg text-black">
-                <h3 className="text-xl font-semibold mb-3">Innovation</h3>
-                <p>
-                Pioneering the future of educational technology with creativity and a forward-thinking mindset.
-                </p>
+            <div className="value-card rounded-lg bg-white p-4 text-black shadow-lg">
+              <h3 className="mb-3 text-xl font-semibold">Innovation</h3>
+              <p>
+                Pioneering the future of educational technology with creativity
+                and a forward-thinking mindset.
+              </p>
             </div>
             {/* Value: Empowerment */}
-            <div className="value-card bg-white p-4 rounded-lg shadow-lg text-black">
-                <h3 className="text-xl font-semibold mb-3">Empowerment</h3>
-                <p>
-                Equipping students with the tools and confidence to take charge of their learning journey.
-                </p>
+            <div className="value-card rounded-lg bg-white p-4 text-black shadow-lg">
+              <h3 className="mb-3 text-xl font-semibold">Empowerment</h3>
+              <p>
+                Equipping students with the tools and confidence to take charge
+                of their learning journey.
+              </p>
             </div>
             {/* Value: Community */}
-            <div className="value-card bg-white p-4 rounded-lg shadow-lg text-black">
-                <h3 className="text-xl font-semibold mb-3">Community</h3>
-                <p>
-                Cultivating a vibrant ecosystem where collaboration and shared success are celebrated.
-                </p>
+            <div className="value-card rounded-lg bg-white p-4 text-black shadow-lg">
+              <h3 className="mb-3 text-xl font-semibold">Community</h3>
+              <p>
+                Cultivating a vibrant ecosystem where collaboration and shared
+                success are celebrated.
+              </p>
             </div>
-            </div>
+          </div>
         </div>
-        </section>
+      </section>
 
-        {/* Meet the Team */}
-        <section className="meet-the-team-section bg-[rgba(0,0,0,0.5)] py-12 my-8">
-        <div className="container mx-auto text-center p-8">
-            <h2 className="text-3xl font-bold text-white mb-8">Meet the Team</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+      {/* Meet the Team */}
+      <section className="meet-the-team-section my-8 bg-[rgba(0,0,0,0.5)] py-12">
+        <div className="container mx-auto p-8 text-center">
+          <h2 className="mb-8 text-3xl font-bold text-white">Meet the Team</h2>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
             {/* Team Member Card */}
-            <Card className="team-member-card bg-white p-4 rounded-lg shadow-lg text-black">
-            <h3 className="text-xl font-semibold mb-2">Dhruv Vaghela</h3>
-            <p className="mb-4">Stevens Institute of Technology</p>
-            <a href="https://www.linkedin.com/in/dhruv-vaghela/" target="_blank" rel="noopener noreferrer" className="text-black hover:text-blue-800">
+            <Card className="team-member-card rounded-lg bg-white p-4 text-black shadow-lg">
+              <h3 className="mb-2 text-xl font-semibold">Dhruv Vaghela</h3>
+              <p className="mb-4">Stevens Institute of Technology</p>
+              <a
+                href="https://www.linkedin.com/in/dhruv-vaghela/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black hover:text-blue-800"
+              >
                 LinkedIn Profile
-            </a>
+              </a>
             </Card>
             {/* Repeat for other team members */}
-            <Card className="team-member-card bg-white p-4 rounded-lg shadow-lg text-black">
-            <h3 className="text-xl font-semibold mb-2">Sanjeet Jain</h3>
-            <p className="mb-4">Stevens Institute of Technology</p>
-            <a href="https://www.linkedin.com/in/sanjeet-jain/" target="_blank" rel="noopener noreferrer" className="text-black hover:text-blue-800">
+            <Card className="team-member-card rounded-lg bg-white p-4 text-black shadow-lg">
+              <h3 className="mb-2 text-xl font-semibold">Sanjeet Jain</h3>
+              <p className="mb-4">Stevens Institute of Technology</p>
+              <a
+                href="https://www.linkedin.com/in/sanjeet-jain/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black hover:text-blue-800"
+              >
                 LinkedIn Profile
-            </a>
+              </a>
             </Card>
-            <Card className="team-member-card bg-white p-4 rounded-lg shadow-lg text-black">
-            <h3 className="text-xl font-semibold mb-2">Yash Baleri</h3>
-            <p className="mb-4">Stevens Institute of Technology</p>
-            <a href="https://www.linkedin.com/in/yashbaleri/" target="_blank" rel="noopener noreferrer" className="text-black hover:text-blue-800">
+            <Card className="team-member-card rounded-lg bg-white p-4 text-black shadow-lg">
+              <h3 className="mb-2 text-xl font-semibold">Yash Baleri</h3>
+              <p className="mb-4">Stevens Institute of Technology</p>
+              <a
+                href="https://www.linkedin.com/in/yashbaleri/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black hover:text-blue-800"
+              >
                 LinkedIn Profile
-            </a>
+              </a>
             </Card>
-            <Card className="team-member-card bg-white p-4 rounded-lg shadow-lg text-black">
-            <h3 className="text-xl font-semibold mb-2">Yuvaraj Nagi</h3>
-            <p className="mb-4">Stevens Institute of Technology</p>
-            <a href="https://www.linkedin.com/in/yuvaraj-nagi-91702b173/" target="_blank" rel="noopener noreferrer" className="text-black hover:text-blue-800">
+            <Card className="team-member-card rounded-lg bg-white p-4 text-black shadow-lg">
+              <h3 className="mb-2 text-xl font-semibold">Yuvaraj Nagi</h3>
+              <p className="mb-4">Stevens Institute of Technology</p>
+              <a
+                href="https://www.linkedin.com/in/yuvaraj-nagi-91702b173/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black hover:text-blue-800"
+              >
                 LinkedIn Profile
-            </a>
+              </a>
             </Card>
-            </div>
+          </div>
         </div>
-        </section>
+      </section>
 
-        {/* Our Story */}
-        <section className="our-story-section container mx-auto bg-[rgba(0,0,0,0.5)] my-8 p-8">
+      {/* Our Story */}
+      <section className="our-story-section container mx-auto my-8 bg-[rgba(0,0,0,0.5)] p-8">
         <div className="container mx-auto">
-            <h2 className="text-3xl font-bold text-center text-white mb-8">Our Story</h2>
-            <p className="text-xl text-white mb-6">
-            Born from the simple need to demystify course selection, UniGrow was conceived by students, for students. 
-            </p>
-            <p className="text-xl text-white mb-6">
-            Our platform emerged from our own struggles to access genuine course insights and connect with peers. It's a testament to the power of shared knowledge and the spirit of collaboration. 
-            </p>
+          <h2 className="mb-8 text-center text-3xl font-bold text-white">
+            Our Story
+          </h2>
+          <p className="mb-6 text-xl text-white">
+            Born from the simple need to demystify course selection, UniGrow was
+            conceived by students, for students.
+          </p>
+          <p className="mb-6 text-xl text-white">
+            Our platform emerged from our own struggles to access genuine course
+            insights and connect with peers. It&apos;s a testament to the power
+            of shared knowledge and the spirit of collaboration.
+          </p>
         </div>
-        </section>
-        <footer className="footer-section text-white py-8">
+      </section>
+      <footer className="footer-section py-8 text-white">
         <div className="container mx-auto text-center">
-            <div className="footer-links grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-12">
-            <a href="/" className="text-base hover:text-white">Home</a>
-            <a href="/faq" className="text-base hover:text-white">FAQ</a>
-            <a href="/aboutus" className="text-base hover:text-white">About</a>
-            <a href="/resources" className="text-base hover:text-white">Resources</a>
-            </div>
-            <p>© {new Date().getFullYear()} UniGrow. All rights reserved.</p>
+          <div className="footer-links mb-12 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+            <a href="/" className="text-base hover:text-white">
+              Home
+            </a>
+            <a href="/faq" className="text-base hover:text-white">
+              FAQ
+            </a>
+            <a href="/aboutus" className="text-base hover:text-white">
+              About
+            </a>
+            <a href="/resources" className="text-base hover:text-white">
+              Resources
+            </a>
+          </div>
+          <p>© {new Date().getFullYear()} UniGrow. All rights reserved.</p>
         </div>
-       </footer>
+      </footer>
     </main>
   );
 }
-
-
--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,14 @@ export default function Home() {
     <div>
       {/* Hero Section */}
 
-      <section className="hero-section container mx-auto text-center py-24">
-        <h1 className="text-5xl font-bold mb-12">Empowering Your Academic Journey</h1>
-        <p className="text-xl mb-8">
-          UniGrow brings the entirety of your campus experience right at your fingertips. Connect, explore, and grow with our comprehensive suite of tools.
+      <section className="hero-section container mx-auto py-24 text-center">
+        <h1 className="mb-12 text-5xl font-bold">
+          Empowering Your Academic Journey
+        </h1>
+        <p className="mb-8 text-xl">
+          UniGrow brings the entirety of your campus experience right at your
+          fingertips. Connect, explore, and grow with our comprehensive suite of
+          tools.
         </p>
         <Link href="/signup">
           <Button className="cta-button">Join Our Community</Button>
@@ -24,24 +28,33 @@ export default function Home() {
         <div className="container mx-auto grid grid-cols-3 gap-8">
           {/* Dynamic Feature Cards */}
           {/* Feature 1: Course Syllabus Integration */}
-          <Card className="feature-card hover:shadow-lg transition duration-300">
-            <h2 className="feature-title text-xl font-semibold mb-4">Syllabus at a Glance</h2>
+          <Card className="feature-card transition duration-300 hover:shadow-lg">
+            <h2 className="feature-title mb-4 text-xl font-semibold">
+              Syllabus at a Glance
+            </h2>
             <p>
-              Automatically synchronize the latest syllabus from Canvas. Stay updated and plan ahead with ease.
+              Automatically synchronize the latest syllabus from Canvas. Stay
+              updated and plan ahead with ease.
             </p>
           </Card>
           {/* Feature 2: Real-time Communication */}
-          <Card className="feature-card hover:shadow-lg transition duration-300">
-            <h2 className="feature-title text-xl font-semibold mb-4">Connect Instantly</h2>
+          <Card className="feature-card transition duration-300 hover:shadow-lg">
+            <h2 className="feature-title mb-4 text-xl font-semibold">
+              Connect Instantly
+            </h2>
             <p>
-              Engage in real-time discussions with peers and mentors. The knowledge you need, shared and received live.
+              Engage in real-time discussions with peers and mentors. The
+              knowledge you need, shared and received live.
             </p>
           </Card>
           {/* Feature 3: User Rating System */}
-          <Card className="feature-card hover:shadow-lg transition duration-300">
-            <h2 className="feature-title text-xl font-semibold mb-4">Peer Insights</h2>
+          <Card className="feature-card transition duration-300 hover:shadow-lg">
+            <h2 className="feature-title mb-4 text-xl font-semibold">
+              Peer Insights
+            </h2>
             <p>
-              Benefit from collective wisdom. Rate and review courses with insights from those who experienced them.
+              Benefit from collective wisdom. Rate and review courses with
+              insights from those who experienced them.
             </p>
           </Card>
           {/* Additional feature cards as necessary */}
@@ -50,15 +63,24 @@ export default function Home() {
 
       <section className="about-section py-12">
         <div className="container mx-auto text-center">
-          <h2 className="text-3xl font-bold mb-4">Our Mission</h2>
-          <p className="text-xl mb-6">
-            We envision a campus ecosystem where information flows freely, collaboration is the norm, and academic resources are readily at hand.
+          <h2 className="mb-4 text-3xl font-bold">Our Mission</h2>
+          <p className="mb-6 text-xl">
+            We envision a campus ecosystem where information flows freely,
+            collaboration is the norm, and academic resources are readily at
+            hand.
           </p>
-          <p className="text-xl mb-6">
-            Our platform is more than just a tool; it's a gateway to unlocking your potential. By seamlessly integrating with Canvas, we provide instant access to course syllabi, facilitate real-time discussions, and offer a robust database of peer-driven course ratings and reviews. 
+          <p className="mb-6 text-xl">
+            Our platform is more than just a tool; it&apos;s a gateway to
+            unlocking your potential. By seamlessly integrating with Canvas, we
+            provide instant access to course syllabi, facilitate real-time
+            discussions, and offer a robust database of peer-driven course
+            ratings and reviews.
           </p>
           <p className="text-xl">
-            Join us on this journey. Embrace the possibilities that lay before you and become a part of a vibrant community dedicated to educational success. With UniGrow, your academic aspirations are within reach.
+            Join us on this journey. Embrace the possibilities that lay before
+            you and become a part of a vibrant community dedicated to
+            educational success. With UniGrow, your academic aspirations are
+            within reach.
           </p>
           <Link href="/aboutus">
             <Button className="cta-button">Learn More</Button>
@@ -66,26 +88,40 @@ export default function Home() {
         </div>
       </section>
 
-      <div className="bg-clip-padding color= "></div>
-
+      <div className="color= bg-clip-padding "></div>
 
       {/* Testimonials Section with Carousel */}
       <section className="testimonials-section bg-black-100 py-12">
         <div className="container mx-auto">
-          <h2 className="text-3xl font-bold text-center mb-8">Success Stories</h2>
+          <h2 className="mb-8 text-center text-3xl font-bold">
+            Success Stories
+          </h2>
           {/* Testimonials Carousel */}
           <div className="testimonial-carousel">
             {/* Individual Testimonials */}
             <blockquote className="testimonial-item">
-              <p>"The course selection process was always daunting, but UniGrow's intuitive platform made it a breeze. The peer reviews and detailed syllabi gave me the confidence to choose the right courses for my career goals."</p>
-              <footer className="testimonial-author">— Yash Kapoor, Master's in Compute Science, Fall 2022</footer>
+              <p>
+                &quot;The course selection process was always daunting, but
+                UniGrow&apos;s intuitive platform made it a breeze. The peer
+                reviews and detailed syllabi gave me the confidence to choose
+                the right courses for my career goals.&quot;
+              </p>
+              <footer className="testimonial-author">
+                — Yash Kapoor, Master&apos;s in Compute Science, Fall 2022
+              </footer>
             </blockquote>
             {/* Additional testimonials */}
             <br />
             {/* Individual Testimonials */}
             <blockquote className="testimonial-item">
-              <p>"Connecting with peers through UniGrow transformed my study sessions. It was incredibly insightful to exchange ideas with those who've walked the path before."</p>
-              <footer className="testimonial-author">— Jane Doe, Master's in Data Science, Spring 2023</footer>
+              <p>
+                &quot;Connecting with peers through UniGrow transformed my study
+                sessions. It was incredibly insightful to exchange ideas with
+                those who&apos;ve walked the path before.&quot;
+              </p>
+              <footer className="testimonial-author">
+                — Jane Doe, Master&apos;s in Data Science, Spring 2023
+              </footer>
             </blockquote>
           </div>
         </div>
@@ -93,9 +129,10 @@ export default function Home() {
 
       <section className="cta-section py-12">
         <div className="container mx-auto text-center">
-          <h2 className="text-4xl font-bold mb-6">Join the UniGrow Family</h2>
+          <h2 className="mb-6 text-4xl font-bold">Join the UniGrow Family</h2>
           <p className="mb-6">
-            Ready to take the next step in your academic career? Become a part of a thriving community that's all about growth and success.
+            Ready to take the next step in your academic career? Become a part
+            of a thriving community that&apos;s all about growth and success.
           </p>
           <Link href="/signup">
             <Button className="cta-button">Start Your Journey</Button>
@@ -109,19 +146,25 @@ export default function Home() {
       py-8: Adds vertical padding.
       
       */}
-      <footer className="footer-section text-white py-8">
+      <footer className="footer-section py-8 text-white">
         <div className="container mx-auto text-center">
-            <div className="footer-links grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-12">
-            <a href="/" className="text-base hover:text-white">Home</a>
-            <a href="/faq" className="text-base hover:text-white">FAQ</a>
-            <a href="/aboutus" className="text-base hover:text-white">About</a>
-            <a href="/resources" className="text-base hover:text-white">Resources</a>
-            </div>
-            <p>© {new Date().getFullYear()} UniGrow. All rights reserved.</p>
+          <div className="footer-links mb-12 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+            <a href="/" className="text-base hover:text-white">
+              Home
+            </a>
+            <a href="/faq" className="text-base hover:text-white">
+              FAQ
+            </a>
+            <a href="/aboutus" className="text-base hover:text-white">
+              About
+            </a>
+            <a href="/resources" className="text-base hover:text-white">
+              Resources
+            </a>
+          </div>
+          <p>© {new Date().getFullYear()} UniGrow. All rights reserved.</p>
         </div>
-       </footer>
+      </footer>
     </div>
   );
 }
-
-
