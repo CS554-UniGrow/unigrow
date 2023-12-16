4891f11e57716c5a7c4c0e8fa35bcc726f931d13
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-12-16 00:41:25 -0500

    Update pusher.ts

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -2,10 +2,10 @@ import PusherServer from "pusher"
 import PusherClient from "pusher-js"
 
 const config = {
-  appId: process.env.PUSHER_APP_ID as string,
+  appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID as string,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
-  secret: process.env.PUSHER_APP_SECRET as string,
-  cluster: process.env.PUSHER_APP_CLUSTER as string,
+  secret: process.env.NEXT_PUBLIC_PUSHER_APP_SECRET as string,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER as string,
   useTLS: Boolean(process.env.NEXT_PUBLIC_PUSHER_APP_TLS!)
 }
 export const pusherServer = new PusherServer({ ...config })
