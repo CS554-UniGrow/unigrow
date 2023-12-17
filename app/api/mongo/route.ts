3ddf781ccb25bcf40cd8a4547df9424729af5c33
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-12-17 15:46:07 -0500

    route fix

--- app/api/mongo/route.ts
+++ app/api/mongo/route.ts
@@ -1,55 +1,60 @@
 import logger from "@/lib/logger"
-import fs from "fs"
+import fs from "fs/promises"
 import { dbConnection, closeConnection } from "@/config/mongo/mongoConnection"
-import { courses as courseCollection } from "@/config/mongo/mongoCollections"
-import { users as userCollection } from "@/config/mongo/mongoCollections"
+import {
+  courses as courseCollection,
+  users as userCollection
+} from "@/config/mongo/mongoCollections"
 import { NextRequest, NextResponse } from "next/server"
-const rawData = fs.readFileSync("./public/course_data_extracted.json", "utf8")
-const seedData = JSON.parse(rawData)
 
 async function seed() {
   try {
+    const filePath = "./lib/course_data_extracted.json" // Adjust the path based on your project structure
+    const rawData = await fs.readFile(filePath, "utf8")
+    const seedData = JSON.parse(rawData)
     const db = await dbConnection()
     logger.info("Connected to the database.")
 
     const courses = await courseCollection()
     const users = await userCollection()
-    await courses.deleteMany({}) // Use deleteMany instead of dropDatabase
+
+    await courses.deleteMany({})
     await users.deleteMany({})
-    logger.info("Collection cleared.")
+    logger.info("Collections cleared.")
+
     await courses.insertMany(seedData)
+
     return "Data seeded successfully."
-  } catch (error) {
-    let message = "Error seeding data:" + error
+  } catch (error: any) {
+    const message = `Error seeding data: ${error.message}`
     logger.error(message)
-    return message
+    throw new Error(message) // Rethrow the error to be caught by the caller
+  } finally {
+    await closeConnection()
+    logger.info("Database connection closed.")
   }
-  // finally {
-  //   await closeConnection();
-  //   logger.info("Database connection closed.");
-  // }
 }
 
 export async function GET(request: NextRequest, response: NextResponse) {
-  const apiKey = request.headers.get("Authorization")
-  logger.info(apiKey)
-  logger.info(`Bearer ${process.env.NEXT_API_SEED_SECRET}`)
+  try {
+    const apiKey = request.headers.get("Authorization")
+    logger.info(apiKey)
+    logger.info(`Bearer ${process.env.NEXT_API_SEED_SECRET}`)
 
-  if (apiKey == `Bearer ${process.env.NEXT_API_SEED_SECRET}`) {
-    logger.info("Seeding data...")
-    const message = await seed()
-    logger.info(message)
-    return NextResponse.json(
-      { message: message, completed: true },
-      { status: 200 }
-    )
-  } else {
-    return NextResponse.json(
-      {
-        message: "Invalid API key",
-        completed: true
-      },
-      { status: 403 }
-    )
+    if (apiKey === `Bearer ${process.env.NEXT_API_SEED_SECRET}`) {
+      logger.info("Seeding data...")
+      const message = await seed()
+      logger.info(message)
+      return NextResponse.json({ message, completed: true }, { status: 200 })
+    } else {
+      return NextResponse.json(
+        { message: "Invalid API key", completed: true },
+        { status: 403 }
+      )
+    }
+  } catch (error: any) {
+    const message = `Error handling request: ${error.message}`
+    logger.error(message)
+    return NextResponse.json({ message, completed: true }, { status: 500 })
   }
 }
