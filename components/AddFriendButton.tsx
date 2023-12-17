1e2683affc1812da460a690e6744d2be08f164bd
Dhruv Vaghela <dhruvvaghela17@gmail.com>
2023-12-17 15:30:08 -0500

    null check fix

--- components/AddFriendButton.tsx
+++ components/AddFriendButton.tsx
@@ -38,7 +38,7 @@ const AddFriendButton = ({
         toast.success("Friend request sent!")
       }
       setIsRequestSent(true)
-    } catch (error) {
+    } catch (error: any) {
       toast.error(error?.response?.data || "An error occurred")
     }
   }
