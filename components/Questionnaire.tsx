d4d41c49bbfc92dc19462a529e4382ba56089b0f
Yash Baleri <baleri.yash@gmail.com>
2023-12-16 20:23:12 -0500

    instruction embed

--- components/Questionnaire.tsx
+++ components/Questionnaire.tsx
@@ -24,10 +24,36 @@ import { useForm } from "react-hook-form";
 import { useSession } from "next-auth/react";
 import { checkCanvasToken, handleSubmitAction } from "@/lib/actions";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import React, { useState, ReactNode } from "react";
 import Loading from "./ui/loading";
 
 import { questionnaireFormSchema, TQuestionnaire } from "@/lib/schemas";
+
+interface FaqItemProps {
+  question: string;
+  answer?: string;
+  children?: ReactNode;
+}
+
+const FaqItem: React.FC<FaqItemProps> = ({ question, answer, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="faq-question py-4">
+      <h3 className="text-base font-semibold mb-8 cursor-pointer" onClick={toggleOpen}>
+        {question}
+      </h3>
+      {isOpen && (
+        <div className="text-base">
+          {children ? children : <p>{answer}</p>}
+        </div>
+      )}
+    </div>
+  );
+};
+
 
 const Questionnaire = () => {
   const { data: session, status, update }: any = useSession();
@@ -153,6 +179,42 @@ const Questionnaire = () => {
                   requires a token, which you can think of as your username and
                   password squished into one long random string. Do not share
                   your token with anyone else!
+                  <br />
+                  <br />
+                  
+                  <FaqItem question="How to Obtain My Canvas Token">
+                    <p>Getting your Canvas key is simple. You can also watch our step-by-step guide on YouTube for detailed instructions.</p>
+
+                    <ol className="list-decimal pl-6">
+                      <li>
+                        Log into Canvas at <a href="https://sit.instructure.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">https://sit.instructure.com</a>.
+                      </li>
+                      <li>
+                        Click &quot;Account&quot; in the left menu, then select &quot;Settings.&quot;
+                      </li>
+                      <li>
+                        Scroll down to &quot;Approved Integration&quot; and click on &quot;New Access Token.&quot;
+                      </li>
+                      <li>
+                        Fill in the &quot;Purpose&quot; field and, for added security, set an expiry date for your token.
+                      </li>
+                      <li>
+                        Click &quot;Generate Token,&quot; then copy and securely save your newly generated token.
+                      </li>
+                    </ol>
+                    {/* Embed a YouTube video */}
+                    <div className="video-container my-4">
+                      <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/_yt8rfD7MTk"
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                      ></iframe>
+                    </div>
+                  </FaqItem>
                 </FormDescription>
                 <FormMessage />
               </FormItem>
