d684d749266c496828451305c4f7cb161a004bff
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-12-21 18:39:42 -0500

    projection to people page

--- app/people/page.tsx
+++ app/people/page.tsx
@@ -31,16 +31,6 @@ function useFetchPeople() {
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
 
-  const { data: session, status }: any = useSession()
-
-  if (!session?.user?.isAuthenticated) {
-    redirect("/signup")
-  }
-
-  if (!session?.user?.isOnboarded) {
-    redirect("/onboarding")
-  }
-
   useEffect(() => {
     setLoading(true)
     fetch("/api/people", { cache: "no-store" })
@@ -59,6 +49,15 @@ function useFetchPeople() {
 }
 
 const People = () => {
+  const { data: session, status }: any = useSession()
+
+  if (!session?.user?.isAuthenticated) {
+    redirect("/signup")
+  }
+
+  if (!session?.user?.isOnboarded) {
+    redirect("/onboarding")
+  }
   const { data, error, loading } = useFetchPeople()
   const [searchQuery, setSearchQuery] = useState("")
 
